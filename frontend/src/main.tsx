a33650c5d01d203fee1a292f74579b6a1d513f52
vow130504 <22120344@student.hcmus.edu.vn>
2025-11-05 14:59:08 +0700

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App.tsx';
-import { AuthProvider } from './hooks/useAuth'; // Import
 
 const queryClient = new QueryClient();
 
